e08aa341b929f30f256b5e89b44811eb653ffe89
rockyzhng <rocky.zhng@gmail.com>
2019-12-27 18:55:28 +0800

    Update ikev2-deploy-psk.sh

--- ikev2-deploy-psk.sh
+++ ikev2-deploy-psk.sh
@@ -94,9 +94,9 @@ iptables -A INPUT -p udp --dport 4500 -j ACCEPT
 
 iptables -A FORWARD --match policy --pol ipsec --dir in  --proto esp -s 10.10.10.10/24 -j ACCEPT
 iptables -A FORWARD --match policy --pol ipsec --dir out --proto esp -d 10.10.10.10/24 -j ACCEPT
-iptables -t nat -A POSTROUTING -s 10.10.10.10/24 -o eth0 -m policy --pol ipsec --dir out -j ACCEPT
-iptables -t nat -A POSTROUTING -s 10.10.10.10/24 -o eth0 -j MASQUERADE
-iptables -t mangle -A FORWARD --match policy --pol ipsec --dir in -s 10.10.10.10/24 -o eth0 -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
+iptables -t nat -A POSTROUTING -s 10.10.10.10/24 -o ens4 -m policy --pol ipsec --dir out -j ACCEPT
+iptables -t nat -A POSTROUTING -s 10.10.10.10/24 -o ens4 -j MASQUERADE
+iptables -t mangle -A FORWARD --match policy --pol ipsec --dir in -s 10.10.10.10/24 -o ens4 -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
 
 iptables -A INPUT -j DROP
 iptables -A FORWARD -j DROP
